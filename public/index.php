3fefa6457003178da065347fb4cb12335dbad3a5
Tendry <tendryzephyrin@gmail.com>
2025-02-08 09:48:39 +0100

    Suppression du slash à la fin de l'URI

--- public/index.php
+++ public/index.php
@@ -5,4 +5,5 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once '../vendor/autoload.php';
 require_once '../Config/Constante.php';
-require_once '../services/Router.php';
+require_once '../services/Router.php';  
+require_once '../config/URI.php';
