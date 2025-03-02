485c154d8c737a4c9b3cc0c0b84369580b40cab0
Tendry <tendryzephyrin@gmail.com>
2025-03-02 21:14:07 +0100

    Création du premier template

--- templates/articles/index.html.php
+++ templates/articles/index.html.php
@@ -8,11 +8,47 @@
 </head>
 <body>
 
-     <?php foreach($articles as $article): ?>
+     <div class="containers">
           <div class="container">
-               <h3><?=$article->nom?></h3>
+               <div class="d-flex align-items-start justify-content-start flex-column gap-2 flex-column">
+                    <div class="container d-flex align-items-center justify-content-between flex-row">
+                         <h2>Les articles</h2>
+                         <a href="" class="btn btn-primary btn-sm">Ajouter</a>
+                    </div>
+                    <form action=""  class="container d-flex align-items-center justify-content-start flex-row gap-2">
+                         <input style="width:15%" type="text" class="form-control" placeholder="Rechercher..." name="search">
+                         <input type="submit" class="btn btn-primary btn-sm" value="Rechercher">
+                    </form>   
+                    <table class="table table-striped">
+                         <thead>
+                              <tr>
+                                   <th>#</th>
+                                   <th>Nom</th>
+                                   <th>Prix</th>
+                                   <th></th>
+                              </tr>
+                         </thead>
+                         <tbody>
+                              <?php foreach($articles as $article): ?>
+                                   <tr>
+                                        <td><?=$article->id?></td>
+                                        <td><?=$article->nom?></td>
+                                        <td><?=$article->prix?></td>
+                                        <td>
+                                             <div class="d-flex gap-1">
+                                                  <a href="" class="btn btn-success btn-sm">Editer</a>
+                                                  <form action="">
+                                                       <input type="submit" class="btn btn-danger btn-sm" value="Supprimer">
+                                                  </form>
+                                             </div>
+                                        </td>
+                                   </tr>
+                              <?php endforeach ?>
+                         </tbody>
+                    </table>
+               </div>
           </div>
-     <?php endforeach ?>
-
+     </div>
+     
 </body>
 </html>
