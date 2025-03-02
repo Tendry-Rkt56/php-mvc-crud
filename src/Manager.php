3837855ad395341cf01b6549eaf009f64964a1db
Tendry <tendryzephyrin@gmail.com>
2025-03-02 20:23:04 +0100

    Création du Manager.php

---
+++ src/Manager.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+use Config\DataBase;
+
+class Manager 
+{
+
+     private static $instance;
+     private static $db;
+     private array $entities = [];
+
+     public function getInstance(): self
+     {
+          if (self::$instance == null) self::$instance = new self();
+          return self::$instance;
+     }
+
+     public function getDb()
+     {
+          if (self::$db == null) self::$db = new DataBase('crud');
+          return self::$db;
+     }
+
+     public function getEntity(string $entity = '')
+     {
+          if (!array_key_exists($entity, $this->entities)) {
+               $this->entities[$entity] = new $entity($this->getDb());
+          }
+          return $this->entities[$entity];
+
+     }
+
+}
