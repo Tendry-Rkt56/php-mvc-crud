115ebb2c1cae56252cf422de1d52cba6e8a13f8d
Tendry <tendryzephyrin@gmail.com>
2025-03-02 21:03:12 +0100

    Configuration de base de tous les applications

--- public/index.php
+++ public/index.php
@@ -5,3 +5,4 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once '../vendor/autoload.php';
 require_once '../Config/Constante.php';
+require_once '../services/Router.php';
