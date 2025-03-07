9a5bf774a62b135cbe5a477a65740e1a52609262
Tendry <tendryzephyrin@gmail.com>
2025-03-07 11:43:03 +0100

    Création des requêtes d'edition

--- templates/categories/index.html.php
+++ templates/categories/index.html.php
@@ -1,14 +1,13 @@
+<?php 
+use Services\Session;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
      <meta charset="UTF-8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <title>Les catégories</title>
-     <?php
-
-use Services\Session;
-
- require_once 'components/head.html' ?>
+     <?php require_once 'components/head.html' ?>
 </head>
 <body>
 
