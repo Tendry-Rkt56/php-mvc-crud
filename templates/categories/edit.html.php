4323ad7cd0ddf5675e1512090636681644ee6099
Tendry <tendryzephyrin@gmail.com>
2025-03-08 07:40:53 +0100

    Modification dans le template d'edition des catégories

--- templates/categories/edit.html.php
+++ templates/categories/edit.html.php
@@ -1,3 +1,6 @@
+<?php
+     use Services\Session;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,6 +16,14 @@
      <div class="forms">
           <form method="POST" style="padding:20px;border-radius:4px;box-shadow:0 0 5px rgba(0,0,0,.2);height:300px;" class="gap-3 d-flex align-items-center justify-content-center flex-column vtsack">
                <h2>Edition</h2>
+               <div class="container">
+                    <?php foreach($_SESSION as $type => $message): ?>
+                         <?php if ($type == 'danger' || $type == "success"): ?>
+                              <div class="container alert alert-<?=$type?> text-center"><?=$message?></div>
+                              <?php Session::delete($type) ?>
+                         <?php endif ?>
+                    <?php endforeach ?>
+               </div>
                <div class="d-flex align-items-center justify-content-center flex-row gap-2 flex-wrap">
                     <input value="<?=$category->nom?>" required type="text" placeholder="Nom de la catégorie..." class="form-control" name="nom">
                     <input type="submit" class="align-self-center my-3 btn btn-primary" value="Ajouter">
